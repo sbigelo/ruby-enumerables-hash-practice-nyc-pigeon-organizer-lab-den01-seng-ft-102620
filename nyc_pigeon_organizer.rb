2e5c666c9915056cd807a1042288002a132de721
Sean Bigelow <sean.bigelow1@gmail.com>
2020-10-09 20:40:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,20 +1,41 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  new_array = data.each_with_object({}) do |(key, value), new_array|
-      value.each do |inner_key, names|
-        names.each do |name|
-      if !new_array[name]
-        new_array[name] = {}
-      end
-      if !new_array[name][key]
-        !new_array[name][key] = []
-      end
-      new_array[name][key].push(inner_key.to_s)
+  new_hash = {}
+  data.each do |key, value|
+    value.each do |color, names|
+      names.each do |each_name|
+        
+    if !new_hash[each_name]
+      new_hash[each_name] = {}
     end
-      new_array
+    if !new_hash[each_name][key]
+      new_hash[each_name][key] = []
     end
+    new_hash[each_name][key] << color.to_s
   end
 end
+end
+new_hash
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
